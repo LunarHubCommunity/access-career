65b5e6475ebc4ef4d43f2d4caa04474f1c324b30
kyy47 <iqyanbim@gmail.com>
2024-05-06 09:27:49 +0800

    slicing: register page

---
+++ src/app/register/page.jsx
@@ -0,0 +1,66 @@
+import Heading from "@/components/micro/Heading";
+import InputAuth from "@/components/micro/InputAuth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Register() {
+  return (
+    <main className="flex items-center justify-center min-h-[100vh]">
+      <div className="flex flex-wrap max-w-4xl shadow-lg min-h-[70vh]">
+        <div className="bg-white flex gap-2 flex-col items-center justify-center min-w-[410px] px-11">
+          <h1 className="font-semibold text-3xl flex text-center leading-tight text-blue-primary">
+            Daftar
+          </h1>
+          <div className="mt-5 mb-4 w-full flex flex-col gap-4">
+            <InputAuth placeholder="Masukkan Email Anda" />
+            <InputAuth placeholder="Masukkan Password Anda" />
+            <InputAuth placeholder="Ulangi Password Anda" />
+          </div>
+          <button className="px-10 py-2 bg-blue-primary rounded-md text-sm text-white hover:shadow-md">
+            DAFTAR
+          </button>
+          <div className="flex gap-1">
+            <p className="font-normal text-blue-primary text-xs ">
+              Sudah punya akun?
+            </p>
+            <Link
+              className="font-bold cursor-pointer text-blue-primary text-xs"
+              href="/login"
+            >
+              Login
+            </Link>
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-[90px] h-[2px] bg-slate-400"></div>
+            <p className="text-xs font-normal text-slate-500">
+              Atau masuk dengan
+            </p>
+            <div className="w-[90px] h-[2px] bg-slate-400"></div>
+          </div>
+          <div className="flex justify-center mt-2 ">
+            <button
+              className="px-4 py-1 -5  border-2
+            border-blue-primary rounded-lg
+            hover:bg-blue-primary hover:border-white hover:shadow-xl"
+            >
+              <Image width={25} height={25} src="/image/icon-google.png" />
+            </button>
+          </div>
+        </div>
+        <div className="min-h-[50vh]  bg-topo  bg-blue-primary flex flex-col justify-center px-10 gap-1">
+          <h3 className="font-medium text-xl text-white">
+            Selamat Datang Kembali Di
+          </h3>
+          <h2 className="text-3xl font-semibold text-white">AccessCareers</h2>
+          <p className="text-sm font-normal text-white max-w-[330px]">
+            Mencerminkan pentingnya memberikan akses yang adil dan setara untuk
+            individu dengan disabilitas dalam dunia kerja.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Register;
