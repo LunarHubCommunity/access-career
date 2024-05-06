0b9bb76b351d8483d7af9ab602f86690f51dc622
kyy47 <iqyanbim@gmail.com>
2024-05-06 09:19:47 +0800

    slicing: login with google

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
 import Heading from "@/components/micro/Heading";
 import InputAuth from "@/components/micro/InputAuth";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -25,7 +26,7 @@ function Login() {
             <InputAuth placeholder="Masukkan Email Anda" />
             <InputAuth placeholder="Masukkan Password Anda" />
           </div>
-          <button className="px-10 py-2 bg-blue-primary rounded-md text-sm text-white">
+          <button className="px-10 py-2 bg-blue-primary rounded-md text-sm text-white hover:shadow-md">
             MASUK
           </button>
           <div className="flex gap-1">
@@ -38,6 +39,22 @@ function Login() {
             >
               Daftar
             </Link>
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-[90px] h-[2px] bg-slate-400"></div>
+            <p className="text-xs font-normal text-slate-500">
+              Atau masuk dengan
+            </p>
+            <div className="w-[90px] h-[2px] bg-slate-400"></div>
+          </div>
+          <div className="flex justify-center mt-2 ">
+            <button
+              className="px-4 py-1 -5  border-2
+            border-blue-primary rounded-lg
+            hover:bg-blue-primary hover:border-white hover:shadow-xl"
+            >
+              <Image width={25} height={25} src="/image/icon-google.png" />
+            </button>
           </div>
         </div>
       </div>
